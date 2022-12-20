f7a9763e99b9ad6e1ce0a2e583fa71498bb061d8
ifnir <theifnir@gmail.com>
2022-12-20 08:57:11 +0100

    script name and displaystringhead

--- dialogues/K#Khae.d
+++ dialogues/K#Khae.d
@@ -3,10 +3,10 @@ BEGIN K#KHAE
 CHAIN IF~True()~ THEN K#KHAE KHAE.00.00
 @0
 DO ~FadeToColor([30.0],0)
-    Wait(2) 
-    ClearAllActions()
-    SetInterrupt(FALSE)
-    StartCutSceneMode()
     Wait(1)
+    ClearAllActions()
     MoveViewPoint([3109.2573],INSTANT)
+    StartCutSceneMode()
     StartCutScene("K#CUT01")~ EXIT
+
+    
