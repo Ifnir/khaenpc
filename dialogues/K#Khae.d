4a2a60422b66d031746c3bd98dfc674087f952ba
ifnir <theifnir@gmail.com>
2022-12-20 07:56:12 +0100

    update sound

--- dialogues/K#Khae.d
+++ dialogues/K#Khae.d
@@ -2,5 +2,11 @@ BEGIN K#KHAE
 
 CHAIN IF~True()~ THEN K#KHAE KHAE.00.00
 @0
-END
-IF~~THEN REPLY @1 EXIT
+DO ~FadeToColor([30.0],0)
+    Wait(2) 
+    ClearAllActions()
+    SetInterrupt(FALSE)
+    StartCutSceneMode()
+    Wait(1)
+    MoveViewPoint([3109.2573],INSTANT)
+    StartCutScene("K#CUT01")~ EXIT
