c8a4e849cd6500670c1fdbf3528114c5e7afdf80
ifnir <theifnir@gmail.com>
2022-12-20 15:44:34 +0100

    update debitor collectors

--- dialogues/K#Khae.d
+++ dialogues/K#Khae.d
@@ -1,6 +1,8 @@
 BEGIN K#KHAE
+BEGIN K#DEB1
 
-CHAIN IF~True()~ THEN K#KHAE KHAE.00.00
+CHAIN 
+IF~Global("K#DEBT","GLOBAL", 0)~ THEN K#KHAE KHAE.00.00
 @0
 DO ~FadeToColor([30.0],0)
     Wait(1)
@@ -8,5 +10,10 @@ DO ~FadeToColor([30.0],0)
     MoveViewPoint([3109.2573],INSTANT)
     StartCutSceneMode()
     StartCutScene("K#CUT01")~ EXIT
+
+CHAIN 
+IF~True()~ THEN K#DEB1 DEB1.01.01
+@1
+EXIT
 
     
