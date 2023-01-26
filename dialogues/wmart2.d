2bf57a9972c02dd66069e73b0191c4c3de7cdea7
Andreas <aka@jma.dk>
2023-01-26 21:11:46 +0100

    update

---
+++ dialogues/wmart2.d
@@ -0,0 +1,8 @@
+EXTEND_TOP WMART2 1 
+++ ~Would you be interested in taking a look at my wares?~ EXTERN WMART2 XWALMART1 
+END 
+
+CHAIN WMART2 XWALMART1 ~I may have some unusual items in the back. Would you like to have a look?~ 
+END 
+++ ~Sure.~ EXIT 
+
