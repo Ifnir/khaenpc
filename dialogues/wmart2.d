1b61eddc89b1974aca1bcbc8ff74ecd304cb5921
Andreas <aka@jma.dk>
2023-01-28 21:35:49 +0100

    update journal

--- dialogues/wmart2.d
+++ dialogues/wmart2.d
@@ -3,7 +3,16 @@ EXTEND_TOP WMART2 1
 END 
 
 CHAIN WMART2 XWALMART1 
-~Shhh. Not so loud. Yes , I'm missing merchandise ordered from you, someone is intercepting the delievering and killing my men.~ 
+~Shhh. Not so loud. Yes , I'm missing merchandise ordered from you, someone is intercepting the delivering and killing my men.~ 
 END 
-++ ~That's no good.~ EXIT 
+++ ~Do you have another deliver coming.~ EXTERN WMART2 XWALMART2
+
+CHAIN WMART2 XWALMART2
+~Yes, this night, at the dock near the lighthouse.~
+END
+++ ~Alright, we'll be taking and personally making sure you recieve the next deliver.~ EXTERN WMART2 XWALMART3
+
+CHAIN WMART2 XWALMART3
+~What a gentleman, the man you'll be meeting is Tom Lee Homes.~
+DO ~SetGlobal("DEIDREIMPORT","GLOBAL",1)~ EXIT
 
