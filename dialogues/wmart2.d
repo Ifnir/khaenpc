67ff1ec9088eacba3d1b5509a4dbd60054097a8d
Andreas <aka@jma.dk>
2023-01-29 19:50:02 +0100

    update

--- dialogues/wmart2.d
+++ dialogues/wmart2.d
@@ -1,5 +1,5 @@
 EXTEND_TOP WMART2 1 
-+~Global("K3GERVQUEST","GLOBAL",3) Global("DEIDREIMPORT","GLOBAL",0)~+ ~We heard you're been missing merchandise lately?~ EXTERN WMART2 XWALMART1 
++~Global("K3GERVQUEST","GLOBAL",3) Global("DEIDREIMPORT","GLOBAL",1)~+ ~We heard you're been missing merchandise lately?~ EXTERN WMART2 XWALMART1 
 END 
 
 CHAIN WMART2 XWALMART1 
@@ -8,11 +8,12 @@ END
 ++ ~Do you have another deliver coming.~ EXTERN WMART2 XWALMART2
 
 CHAIN WMART2 XWALMART2
-~Yes, this night, at the dock near the lighthouse.~
+~Yes, at night, in the dock district next to the blacksmith.~
 END
 ++ ~Alright, we'll be taking and personally making sure you recieve the next deliver.~ EXTERN WMART2 XWALMART3
 
 CHAIN WMART2 XWALMART3
-~What a gentleman, the man you'll be meeting is Tom Lee Homes.~
-DO ~SetGlobal("DEIDREIMPORT","GLOBAL",1)~ EXIT
+~What a gentleman, you'll be meeting the shadow thieves.~
+DO ~SetGlobal("DEIDREIMPORT","GLOBAL",2)~ 
+EXIT
 
