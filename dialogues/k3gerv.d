a203ff9f81e1f2f0dcf63bd88556139bbbf41c0c
Andreas <aka@jma.dk>
2023-02-02 21:20:27 +0100

    update

--- dialogues/k3gerv.d
+++ dialogues/k3gerv.d
@@ -28,29 +28,29 @@ IF~~THEN REPLY @315 DO ~SetGlobal("K3GERVQUEST","GLOBAL",1)~ EXIT
 
 // New quest to meet deidre in adventure mart SetGlobal("K3GERVQUEST","GLOBAL",3)
 CHAIN IF~Global("TALKTOGERV2","GLOBAL",1)~ THEN K3GERV QUEST2
-~You're back, splendid. Did you get anything out of Zhal?~
+~Ahh, look who's returned! Simply splendid, my comrade. And tell me, did you have any luck in your dealings with Zhal? Did you manage to uncover anything of note?~
 END
 +~Global("ZHALLQUEST","GLOBAL",1)~+ ~I did, it wasn't much, however, he told me about someone moving in and out of the broken cell.~ EXTERN K3GERV QUEST2.1
 +~Global("K3EBRIFQUEST","GLOBAL",3)~+ ~No, but I meet Ebrif, the one who was in the prison cell and escaped. I helped him escape.~ EXTERN K3GERV QUEST2.1.1
 
 CHAIN K3GERV QUEST2.1.1
-~Ohhh, that's not good for business, how did he escape exactly, he couldn't have done all by himself?~
+~Oh my, this is most unfortunate for our line of work, isn't it? How, might I ask, did this individual manage to make his escape? It seems implausible that he could have accomplished such a feat all on his own, don't you think?~
 END
 ++ ~Ohhh, I did. It never occurred me to question how exactly he managed to dig out, he couldn't have done it without help.~
 EXTERN K3GERV QUEST2.2
 
 CHAIN K3GERV QUEST2.1
-~Wasn't it occupied, what happen to that guy, did he by chance escape, because I believe someone might assisted him.~
+~Well now, wasn't that location supposed to be occupied? What could have become of the individual in question? Did he perhaps escape on his own, or could it be that someone might have provided him with a helping hand, if you catch my drift?~
 END
 ++ ~Ohhh, I did. It never occurred me to question how exactly he managed to dig out, he couldn't have done it without help.~
 EXTERN K3GERV QUEST2.2
 
 CHAIN K3GERV QUEST2.2
-~Well, no worries. Let's move on, I've recently found out one of our close customer, Deidre, is missing her contraband. 
-Her men is continous getting high-jacked and in some instances killed. I need you to go talk to her and resolve this matter, it might let us closer to any clue to who is the traitor among us.~
+~Ahh, no need to fret, my comrade. Let us move forward, shall we? I have just received news that one of our valued clients, the lovely Deidre, has been experiencing some difficulties with her contraband. Her shipments have been repeatedly high-jacked, and there have even been instances of fatalities. This simply will not do. I need you to pay her a visit and see what can be done to resolve this issue. Who knows, perhaps this will lead us closer to discovering the traitor in our midst.~
 DO ~SetGlobal("K3GERVQUEST","GLOBAL",3) SetGlobal("DEIDREIMPORT","GLOBAL",1)~ EXIT
 
 CHAIN IF ~~ THEN K3GERV QUEST3
-~You're back, what did you learn.~
+~Ahh, welcome back! And tell me, what have you learned on your journey? Did you manage to be of assistance to our dear friend Deidre and help resolve the difficulties she has been facing with her contraband?~
+== K3XAV ~Listen closely, Gervel and <CHARNAME>. The shadow thieves you encountered at the blacksmith, who was supposed to be an ally of ours, they came at you with a vengeance, in league with those bloodthirsty vampires. It's a treacherous situation, one that has put us in a compromising position. These individuals, who were supposed to have our trust, have now aligned themselves with our enemies and it's important that we take swift action to protect ourselves from their malicious intent.~
 EXIT
 
