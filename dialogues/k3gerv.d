06bf2abf3b2a47d3688b4dae40b7cd12fcb9f581
Andreas <aka@jma.dk>
2023-02-07 14:27:05 +0100

    update

--- dialogues/k3gerv.d
+++ dialogues/k3gerv.d
@@ -27,7 +27,7 @@ END
 IF~~THEN REPLY @315 DO ~SetGlobal("K3GERVQUEST","GLOBAL",1)~ EXIT
 
 // New quest to meet deidre in adventure mart SetGlobal("K3GERVQUEST","GLOBAL",3)
-CHAIN IF~Global("TALKTOGERV2","GLOBAL",1)~ THEN K3GERV QUEST2
+CHAIN IF~Global("TALKTOGERV2","GLOBAL",1) Global("DEIDREIMPORT","GLOBAL",0) Global("K3GERVQUEST","GLOBAL",2)~ THEN K3GERV QUEST2
 ~Ahh, look who's returned! Simply splendid, my comrade. And tell me, did you have any luck in your dealings with Zhal? Did you manage to uncover anything of note?~
 END
 +~Global("ZHALLQUEST","GLOBAL",1)~+ ~I did, it wasn't much, however, he told me about someone moving in and out of the broken cell.~ EXTERN K3GERV QUEST2.1
@@ -49,8 +49,14 @@ CHAIN K3GERV QUEST2.2
 ~Ahh, no need to fret, my comrade. Let us move forward, shall we? I have just received news that one of our valued clients, the lovely Deidre, has been experiencing some difficulties with her contraband. Her shipments have been repeatedly high-jacked, and there have even been instances of fatalities. This simply will not do. I need you to pay her a visit and see what can be done to resolve this issue. Who knows, perhaps this will lead us closer to discovering the traitor in our midst.~
 DO ~SetGlobal("K3GERVQUEST","GLOBAL",3) SetGlobal("DEIDREIMPORT","GLOBAL",1)~ EXIT
 
-CHAIN IF ~~ THEN K3GERV QUEST3
+CHAIN IF ~Global("DEIDREIMPORT","GLOBAL",4) Global("K3WIZARD","GLOBAL",0)~ THEN K3GERV QUEST3
 ~Ahh, welcome back! And tell me, what have you learned on your journey? Did you manage to be of assistance to our dear friend Deidre and help resolve the difficulties she has been facing with her contraband?~
-== K3XAV ~Listen closely, Gervel and <CHARNAME>. The shadow thieves you encountered at the blacksmith, who was supposed to be an ally of ours, they came at you with a vengeance, in league with those bloodthirsty vampires. It's a treacherous situation, one that has put us in a compromising position. These individuals, who were supposed to have our trust, have now aligned themselves with our enemies and it's important that we take swift action to protect ourselves from their malicious intent.~
-EXIT
+END
+++ ~We were ambushed by vampires, and the shadow thieves we was supposdly to meet, assisted the vampires.~ EXTERN K3XAV K3XAV1
+
+CHAIN K3XAV K3XAV1
+~Listen closely, Gervel and <CHARNAME>. The shadow thieves you encountered at the blacksmith, who was supposed to be an ally of ours, they came at you in league with those bloodthirsty vampires.~
+== K3XAV ~It's a treacherous situation, one that has put us in a compromising position. These individuals, who were supposed to have our trust, have now aligned themselves with our enemies and it's important that we take swift action to protect ourselves from their malicious intent.~
+== K3GERV ~It's unfortunate, more and more is leaving the shadow thieves in fear, and converting to those beasts. Go to our wizard, he knows what we've to do.~
+DO ~SetGlobal("K3WIZARD","GLOBAL",1)~ EXIT
 
---
+++ dialogues/k3verd.d
@@ -0,0 +1,46 @@
+CHAIN IF ~Global("K3WIZARD","GLOBAL",1)~ THEN K3VERD VERD1
+~Oh, <CHARNAME>, about time. For your information I'm well aware of the situation, and I already have a plan prepared.~
+END
+++ ~What's the plan?~ EXTERN K3VERD VPLAN
+++ ~Rather suspicious, tell me exactly, what you know of our *situation*.~ EXTERN K3VERD VEXCUSE1
+
+CHAIN K3VERD VEXCUSE1
+~Excuse me, I'm a well-respected within the organization. My job is to know everything, I already had my theories, but I couldn't be sure. Thankfully you confirmed I was right all along.~
+END
+++ ~Is that so, well, I suppose you oughta know.~ EXTERN K3VERD VPLAN
+
+CHAIN K3VERD VPLAN
+~At the graveyard, find the the tomb to the southeast. In there is a face made of clay facing toward you. You will cycle these instruction, I've yet to decipher them, so that will be up to you.~
+END
+++ ~Why haven't you deciphered the instruction yourself?~ EXTERN K3VERD VPLANQ1
+++ ~What we're up against?~ EXTERN K3VERD VPLANQ2
+++ ~Affirmative.~ EXTERN K3VERD VPLANGO
+
+CHAIN K3VERD VPLANGO
+~Excellent, now go away and do my bidding.~
+DO ~Global("K3WIZARD","GLOBAL",2)~ EXIT
+
+CHAIN K3VERD VPLANQ1
+~I don't have time to do everything, I've other inquiries, that need my attention.~ EXTERN K3VERD VPLAN
+
+CHAIN K3VERD VPLANQ2
+~From what I research, you'll be transported to a dimesion of the undead realm. In there is an weapon, we can use against the vampires, where exactly is unknown to me.~ EXTERN K3VERD VPLAN
+
+// CHAIN K3VERD VPLAN
+// ~~ EXTERN K3VERD VPLAN
+
+
+// CHAIN K3VERD VPLAN
+// ~~
+// END
+
+
+// Change the start conversation depending if you helped Zhal or let Ebrif escape
+// The quest is Verdel has a book with instruction, where you have to go the graveyard 811 and
+// Click on the face, do the instruction, then a portal opens, where you have to fight a evil spirit.
+// then you get a item, that verdel need, he lied to you about, because this item 
+// will not help the fight against the vampires, instead it will turn him to a lich.
+// He will then fight bror in the main hall, assisted by vampires
+// 
+// You can either accept to help him remove him, or fight him.
+// Before you give him the lich item, you can go to gervel, he will have you go to Ula, she will look and tell you the truth.
